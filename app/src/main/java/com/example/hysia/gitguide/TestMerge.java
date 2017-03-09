957849de4398df475aa6c5aa941e73d39de0ae87
zum <hysia.assist@gmail.com>
2017-03-09 09:59:49 +0900

    [ hysia ]  develop branch class add

---
+++ app/src/main/java/com/example/hysia/gitguide/TestMerge.java
@@ -0,0 +1,16 @@
+package com.example.hysia.gitguide;
+
+/**
+ * Created by hysia on 2017. 3. 9..
+ */
+
+public class TestMerge {
+
+    public TestMerge() {
+    }
+
+    @Override
+    public String toString() {
+        return "TestMerge{}";
+    }
+}
